c22d229703a812afbbb5a676466a55eacbd5ddfc
srinivas <selenium1.testing1@gmail.com>
2023-05-23 21:20:02 +0100

    changed loginfiles

---
+++ src/test/java/SampleGit/SampleGittest/Loginpage.java
@@ -0,0 +1,12 @@
+package SampleGit.SampleGittest;
+
+public class Loginpage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("login page");
+	}
+
+}
